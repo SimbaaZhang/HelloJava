b9300b1ae411f2e4931c027abe1496acaafd0167
SimbaaZhang <simbaazhang@outlook.com>
2018-09-05 00:15:59 +0800

    start learning java

---
+++ LearningJava/src/HelloJava/HelloJava.java
@@ -0,0 +1,12 @@
+package HelloJava;
+
+import java.util.*;
+
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello it is :");
+		System.out.println(new Date());
+	} 
+	
+}
